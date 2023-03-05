4ce60873b2688d7f08311a0d00221f606fcfd647
jlbuild <juliabuildbot@gmail.com>
2023-03-05 20:01:20 +0000

    Qt65Compat_jll build 6.4.2+0

---
+++ src/Qt65Compat_jll.jl
@@ -0,0 +1,9 @@
+# Use baremodule to shave off a few KB from the serialized `.ji` file
+baremodule Qt65Compat_jll
+using Base
+using Base: UUID
+import JLLWrappers
+
+JLLWrappers.@generate_main_file_header("Qt65Compat")
+JLLWrappers.@generate_main_file("Qt65Compat", UUID("f5784262-74e5-52be-b835-f3e8a3cf8710"))
+end  # module Qt65Compat_jll
