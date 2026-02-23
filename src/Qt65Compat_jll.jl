40072b06d73b6181c65a5f4cef0e3350abbf57cc
jlbuild <juliabuildbot@gmail.com>
2026-02-23 13:25:50 +0000

    Qt65Compat_jll build 6.10.2+0

--- src/Qt65Compat_jll.jl
+++ src/Qt65Compat_jll.jl
@@ -5,5 +5,5 @@ using Base: UUID
 import JLLWrappers
 
 JLLWrappers.@generate_main_file_header("Qt65Compat")
-JLLWrappers.@generate_main_file("Qt65Compat", UUID("f5784262-74e5-52be-b835-f3e8a3cf8710"))
+JLLWrappers.@generate_main_file("Qt65Compat", Base.UUID("f5784262-74e5-52be-b835-f3e8a3cf8710"))
 end  # module Qt65Compat_jll
